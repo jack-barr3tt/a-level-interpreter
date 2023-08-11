d2fe81b85d940d825113c8f889746292ad8366ec
Jack Barrett <jjbdev21@gmail.com>
2023-08-11 17:33:55 +0100

    Clean up logging

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -2,7 +2,6 @@
 
 #include <string>
 #include <vector>
-#include <iostream>
 #include <regex>
 
 std::string Lexer::generatePattern() {
@@ -30,18 +29,11 @@ std::vector<Token> Lexer::tokenize(std::string inputString) {
   while (std::regex_search(inputString, match, regexObj)) {
     if(match.empty()) continue;
 
-    std::cout << "Tokens: " << tokens.size() << std::endl;
-
     std::string matchString = match[0];
 
-    std::cout << "Match string: " << matchString << std::endl;
-
     for(int i = 0; i < Token::Type::Count; i++) {
-      std::cout << "Token: " << i << std::endl;
       Token temp((Token::Type)i, matchString);
-      std::cout << "Pattern: " << temp.pattern() << std::endl;
       if(std::regex_match(matchString, std::regex(temp.pattern()))) {
-        std::cout << temp.text() << std::endl;
         tokens.push_back(temp);
         break;
       }
