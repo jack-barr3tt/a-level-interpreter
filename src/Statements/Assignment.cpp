4098446d61c9525e44fc3906fcf3d7d5868e6fd1
Jack Barrett <jjbdev21@gmail.com>
2023-09-01 21:58:47 +0100

    Code cleanup

---
+++ src/Statements/Assignment.cpp
@@ -0,0 +1,11 @@
+#include "Assignment.h"
+
+void Assignment::execute() {
+  memory->add(identifier, expression->evaluate());
+}
+
+Assignment::Assignment(std::shared_ptr<Memory> memory, std::string identifier, std::shared_ptr<Expression> expression) {
+  this->memory = memory;
+  this->identifier = identifier;
+  this->expression = std::move(expression);
+}
--- src/Statements/Assignment.h
+++ src/Statements/Assignment.h
@@ -13,13 +13,3 @@ public:
   Assignment(std::shared_ptr<Memory> memory, std::string identifier, std::shared_ptr<Expression> expression);
   void execute() override;
 };
-
-void Assignment::execute() {
-  memory->add(identifier, expression->evaluate());
-}
-
-Assignment::Assignment(std::shared_ptr<Memory> memory, std::string identifier, std::shared_ptr<Expression> expression) {
-  this->memory = memory;
-  this->identifier = identifier;
-  this->expression = std::move(expression);
-}
